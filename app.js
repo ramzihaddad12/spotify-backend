d61d557831e2d1675985907ef58a9b0510ea4a38
Ramzi Haddad <ramzihaddad@Ramzis-MacBook-Air.local>
2023-04-14 10:46:41 -0400

    deploy fix#3

--- app.js
+++ app.js
@@ -9,25 +9,23 @@ import AlbumsController from "./controllers/albums/albums-controller.js";
 import PlaylistController from "./controllers/playlist/playlist-controller.js";
 
 const app = express();
-app.enable('trust proxy')
 
 app.set('trust proxy', 1);
+
 app.use(express.json());
+
 app.use(session({
     resave: false,
     saveUninitialized: true,
     secret: 'my-secret',
     cookie: { secure: true }
 }));
+
 app.use(cors({
     credentials: true,
     origin: 'https://flourishing-tanuki-d1c818.netlify.app'
 }));
-app.enable('trust proxy')
-
-app.set('trust proxy', 1)
-// sess.cookie.secure = true;
-
+app.set('trust proxy', 1);
 
 mongoose.connect('mongodb+srv://Cluster21145:Cluster21145@cluster21145.yc3qyis.mongodb.net/test', { useNewUrlParser: true });
 UserController(app)

46d5a4e8b24ce61128eab9d5b64084b3b53a9355
Ramzi Haddad <ramzihaddad@Ramzis-MacBook-Air.local>
2023-04-14 13:17:47 -0400

    changed cors origin #4

--- app.js
+++ app.js
@@ -9,31 +9,28 @@ import AlbumsController from "./controllers/albums/albums-controller.js";
 import PlaylistController from "./controllers/playlist/playlist-controller.js";
 
 const app = express();
-app.set('trust proxy', 1);
-app.use(express.json());
+
 // app.use(session({
 //     resave: false,
 //     saveUninitialized: true,
 //     secret: 'sdlfjljrowuroweu',
 //     cookie: { secure: false },
 // }));
-app.set('trust proxy', 1);
+app.use(
+    session({
+        secret: "any string",
+        resave: false,
+        saveUninitialized: true,
+    })
+);
 
-app.use(session({
-    cookie:{
-        secure: false,
-        maxAge:60000
-    },
-    // store: new RedisStore(),
-    secret: 'secret',
-    saveUninitialized: true,
-    resave: true
-}));
 app.use(cors({
     credentials: true,
     origin: 'https://flourishing-tanuki-d1c818.netlify.app' //'http://localhost:3000'
 }));
-app.set('trust proxy', 1)
+app.use(express.json());
+
+app.set('trust proxy', 1);
 
 mongoose.connect('mongodb+srv://Cluster21145:Cluster21145@cluster21145.yc3qyis.mongodb.net/test', { useNewUrlParser: true });
 UserController(app)

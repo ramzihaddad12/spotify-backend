2ee23044de12b188a76e6d13b91314ec705846c7
Ramzi Haddad <ramzihaddad@Ramzis-MacBook-Air.local>
2023-04-15 11:51:38 -0400

    code cleanup

--- app.js
+++ app.js
@@ -9,42 +9,10 @@ import UserController from "./controllers/users/users-controller.js"
 import SongsController from "./controllers/songs/songs-controller.js";
 import AlbumsController from "./controllers/albums/albums-controller.js";
 import PlaylistController from "./controllers/playlist/playlist-controller.js";
-import MongoStore from "connect-mongo";
-// const app = express();
-//
-// // app.use(session({
-// //     resave: false,
-// //     saveUninitialized: true,
-// //     secret: 'sdlfjljrowuroweu',
-// //     cookie: { secure: false },
-// // }));
-// // app.set('trust proxy', 1);
-//
-// app.use(
-//     session({
-//         // store: new MongoStore({
-//         //     url: 'mongodb+srv://Cluster21145:Cluster21145@cluster21145.yc3qyis.mongodb.net/test',
-//         //     ttl: 14 * 24 * 60 * 60, // session expiration time in seconds
-//         //     autoRemove: 'native', // automatically remove expired sessions
-//         //     collectionName: 'sessions' // collection name for the sessions
-//         // }),
-//         secret: "any string",
-//         resave: false,
-//         saveUninitialized: true,
-//         cookie: {
-//             secure: false
-//         }
-//     })
-// );
-//
-// app.use(cors({
-//     credentials: true,
-//     origin: 'http://localhost:3000' //'https://flourishing-tanuki-d1c818.netlify.app' //
-// }));
-// app.use(express.json());
+
 const app = express();
 const sess = session.SessionOptions = {
-    secret: "process.env.SECRET",
+    secret: "my-secret",
     resave: false,
     saveUninitialized: true,
     cookie: { secure: false },
@@ -55,7 +23,7 @@ if (process.env.ENV === 'PROD') {
     sess.cookie.sameSite = 'none';
 }
 app.use(session(sess));
-app.use(cors({ credentials: true, origin: "https://flourishing-tanuki-d1c818.netlify.app" }));
+app.use(cors({ credentials: true, origin: process.env.ORIGIN }));
 app.use(bodyParser.json());
 
 mongoose.connect('mongodb+srv://Cluster21145:Cluster21145@cluster21145.yc3qyis.mongodb.net/test', { useNewUrlParser: true });
